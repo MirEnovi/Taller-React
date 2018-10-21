d50d22d62f60921c2614852ce5cc46c9c94c76d0
MirEnovi <garciaivonne11@gmail.com>
2018-10-20 22:53:38 -0500

    Updates

---
+++ precache-manifest.0fce50974dc8eda369d3a0633557795f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c453b51bea176775f96f",
+    "url": "/Taller-React/static/js/runtime~main.c453b51b.js"
+  },
+  {
+    "revision": "6347b216eb9fd373b1f5",
+    "url": "/Taller-React/static/js/main.6347b216.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/Taller-React/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "6347b216eb9fd373b1f5",
+    "url": "/Taller-React/static/css/main.a219a697.chunk.css"
+  },
+  {
+    "revision": "c3683e9d67b9e1a752938463fd6e6670",
+    "url": "/Taller-React/index.html"
+  }
+];
